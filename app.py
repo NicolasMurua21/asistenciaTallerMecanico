bbf783d23eb30ab5dba8ef2f7ea2d35f75d1b98f
Nicolas Murua <murua7816@gmail.com>
2026-09-21 15:56:28 -0300

    Creacion del endpoint corregir asistencia

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-##ACA VA TODA LA LOGICA QUE TIENE QUE VER CON LA API DE FLASK
+##ACA VA TODA LA LOGICA QUE TIENE QUE VER CON LA API DE FASTAPI
+from fastapi import FastAPI, Request
+from administrador import Administrador
+import baseDatos
+
+DB = baseDatos.inicializar_base_datos()
+app = FastAPI()
+
+
+@app.patch("/api/admin/empleados/{dni}/asistencias/{fechaEmpleado}")
+async def corregir_asistencia(dni, fechaEmpleado, request: Request):
+    datos = await request.json()
+
+    if not datos:
+        return "No se enviaron datos"
+
+    camposModificar = datos.pop("camposModificar", [])
+    resultado = Administrador.corregirRegistro(DB, dni, fechaEmpleado, camposModificar, **datos)
+
+    return {"mensaje": "Procesado con éxito", "resultado": resultado}
